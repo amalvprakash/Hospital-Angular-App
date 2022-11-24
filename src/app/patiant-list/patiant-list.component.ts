b134da17d967210811e4488e554921686e262da1
Amal <amalprakash2025@gmail.com>
2022-11-24 12:53:45 +0530

    Added Student Lists

---
+++ src/app/patiant-list/patiant-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patiant-list',
+  templateUrl: './patiant-list.component.html',
+  styleUrls: ['./patiant-list.component.css']
+})
+export class PatiantListComponent {
+
+}
